392a7103bb3cf2bb942f257671c9fa39e7de730a
dkrleza <krleza.dalibor@gmail.com>
2022-07-27 20:07:34 +0200

    Updates and Trie

--- Trie.py
+++ Trie.py
@@ -1,28 +1,23 @@
-
 class TrieNode:
     def __init__(self,p=None):
-        self.children=[]
+        self.children={}
         self.parent=p
-
-class TrieNode(TrieNode):
     def isLeaf(self) -> (bool):
         return len(self.children)==0
-    def transition(self,c:chr) -> (bool,TrieNode):
-        for (ct,cn) in self.children:
-            if c==ct:
-                return (True,cn)
+    def transition(self,c:chr) -> (bool,object):
+        if c in self.children: return (True,self.children[c])
         return (False,None)
-    def insert(self,c:chr) -> (TrieNode):
+    def insert(self,c:chr) -> (object):
         (ct,cn)=self.transition(c)
         if not ct:
             ncn=TrieNode(self)
-            self.children.append((c,ncn))
+            self.children[c]=ncn
             return ncn
         else: return cn
     def remove(self,c:chr) -> (bool):
         (ct,cn)=self.transition(c)
         if ct:
-            self.children.remove((c,cn))
+            del self.children[c]
             return True
         return False
 
@@ -50,8 +45,6 @@ class Trie:
                 currTN.remove(c)
                 if split: break
                 currTN=currTN.parent
-                if currTN is None: break # we are at the root node
+                if currTN is None: break
             return True
         return False
-
-
